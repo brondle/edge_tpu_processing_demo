d7ad76a7d482fed01c63d51ea1d4d1a7c6171661
Adi Dahiya <adi.dahiya14@gmail.com>
2019-05-04 03:17:57 -0400

    simplify face_detection_socket

--- face_detection_socket.py
+++ face_detection_socket.py
@@ -43,9 +43,9 @@ def main():
 
     print('listening...')
 
-    _, width, height, channels = engine.get_input_tensor_shape()
+    _, width, height, _ = engine.get_input_tensor_shape()
 
-    imageSize = width*height*3
+    # imageSize = width*height*3
 
     print('waiting for client')
 
@@ -62,23 +62,18 @@ def main():
         if (len(data) > 0):
             start_s = time.time()
 
-            #  print('processing image')
             try:
                 image = Image.open(io.BytesIO(data)).convert('RGB')
             except OSError:
                 print('Could not read image')
                 continue
 
-            # inputTensor = np.frombuffer(image.tobytes(), dtype=np.uint8)
-
             results = engine.DetectWithImage(
                 image, threshold=0.25, keep_aspect_ratio=True, relative_coord=False, top_k=3)
-            # results = engine.DetectWithInputTensor(inputTensor, threshold=0.25,
-            #                                        top_k=10)
 
             print('time to process image', (time.time() - start_s) * 1000)
 
-            output = to_output(results, image.size)
+            output = to_output(results)
 
             message = json.dumps({'results': output}) + '|'
             print(message)
@@ -92,31 +87,14 @@ def main():
 
             #  receiveSocket.sendto(message.encode('utf-8'), addr)
             #  senderSocket.sendto(message.encode('utf-8'), (UDP_IP, UDP_SEND_PORT))
-          #  receivedBytes=bytearray()
-
-        #  start_s = time.time()
-
-        # Run inference.
-        #  results = engine.DetectWithInputTensor(input, threshold=0.25,
-                #  top_k=10)
-        #  elapsed_s = time.time() - start_s
 
     #  conn.close()
 
 
-def to_output(results, full_size):
+def to_output(results):
     return list(map(lambda result: {
-        'box': scale_box(result.bounding_box, full_size),
+        'box': result.bounding_box.flatten().tolist(),
     }, results))
-
-
-def scale_boxes(results, full_size):
-    return list(map(lambda result:
-                    (scale_box(result.bounding_box, full_size)).tolist(), results))
-
-
-def scale_box(box, full_size):
-    return (box * (full_size[0], full_size[1])).flatten().tolist()
 
 
 if __name__ == '__main__':
